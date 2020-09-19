9e5df3765370037483d787fcda2103b7febe8595
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-09-19 01:54:03 -0400

    Add shared methods

--- src/main/java/com/comp445/httpc/HttpcRequest.java
+++ src/main/java/com/comp445/httpc/HttpcRequest.java
@@ -1,48 +1,89 @@
 package com.comp445.httpc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
 import java.net.Socket;
-import java.net.InetAddress;
-import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 public abstract class HttpcRequest {
-    public URL url;
-    public Host host;
-    public Map<String, Object> headers;
+    /**
+     * This class represents a request made by httpc. Only GET/POST requests are to
+     * be supported.
+     * 
+     * Methods implemented follow https://tools.ietf.org/html/rfc7230#section-3.2
+     */
+    protected Host host;
+    protected Map<String, String> headers;
+    protected Socket socket;
+    protected BufferedReader in;
+    protected PrintWriter out;
+    protected final int HTTP_PORT = 80;
 
-    protected HttpcRequest(URL url, Host host, Map<String, Object> headers) {
-        this.url = url;
+    protected HttpcRequest(final Host host, final Map<String, String> headers) {
         this.host = host;
         this.headers = headers;
     }
 
-    public abstract void connect();
+    protected abstract void setDataHeaders();
 
-    public String usageGeneral() {
-        return "Usage:\n"
-                .concat("   httpc command [arguments]\n")
-                .concat("The commands are:\n")
-                .concat("   get     executes a HTTP GET request and prints the response.\n")
-                .concat("   post    executes a HTTP POST request and prints the response.\n")
-                .concat("   help    prints this screen.\n")
-                .concat("\nUse \"httpc help [command]\" for more information about a command.");
+    protected abstract String getRequestType();
+
+    protected String connect() {
+        try {
+            socket = new Socket(host.url.getHost(), HTTP_PORT);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            setRequestHeaders();
+            setDataHeaders();
+            out.printf("\r%n");
+
+            final String data = readData();
+            if (Httpc.isVerbose) {
+                System.out.println(data);
+            }
+            close();
+            return data;
+        } catch (final MalformedURLException e) {
+            System.out.println(e.getMessage());
+        } catch (final UnknownHostException e) {
+            System.out.println(e.getMessage());
+        } catch (final IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return "An error occurred!";
     }
 
-    public String usageGet() {
-        return "httpc help get\n"
-                .concat("   usage: httpc get [-v] [-h key:value] URL\n")
-                .concat("Get executes a HTTP GET request for a given URL.\n")
-                .concat("   -v      Prints the detail of the response such as protocol, status, and headers.\n")
-                .concat("   -h key:value    Associates headers to HTTP Request with the format 'key:value'.");
+    protected String readData() throws IOException {
+        int character;
+        final StringBuilder container = new StringBuilder();
+
+        while ((character = in.read()) != -1) {
+            container.append((char) character);
+        }
+        return container.toString();
     }
 
-    public String usagePost() {
-        return "httpc help post\n".concat("    usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n")
-                .concat("Post executes a HTTP POST request for a given URL with inline data or from file.\n")
-                .concat("   get     executes a HTTP GET request and prints the response.\n")
-                .concat("   post    executes a HTTP POST request and prints the response.\n")
-                .concat("   help    prints this screen.\n")
-                .concat("Use \"httpc help [command]\" for more information about a command.");
+    private void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+
+    protected String getQueryIfPresent() {
+        return (!host.url.getQuery().equals("") && !host.url.getQuery().equals("/")) ? "?" + host.url.getQuery() : "";
+    }
+
+    protected void setRequestHeaders() {
+        out.printf("%s %s%s  HTTP/1.1\r%n", getRequestType(), host.url.getPath(), getQueryIfPresent());
+        out.printf("Host: " + host.url.getHost() + "\r%n");
+        out.printf("Upgrade-Insecure-Requests: 1\r%n");
+        out.printf("Connection: Close\r%n");
+        out.printf("DNT: 1\r%n");
     }
 
 }
