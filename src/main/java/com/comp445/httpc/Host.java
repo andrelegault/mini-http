8b0b8ba96540519ff1251262a4119d54df9bc8aa
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-09-19 00:02:19 -0400

    Only create URL

--- src/main/java/com/comp445/httpc/Host.java
+++ src/main/java/com/comp445/httpc/Host.java
@@ -1,30 +1,15 @@
 package com.comp445.httpc;
 
-import java.io.OutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.Socket;
 import java.net.URL;
-import java.net.UnknownHostException;
 
 public class Host {
-    public Socket socket;
-    private URL url;
-    public OutputStream outputStream;
-    public InputStream inputStream;
+    protected URL url;
 
     public Host(String endpoint) {
         try {
-            url = new URL(endpoint);
-            socket = new Socket(url.getHost(), 80);
-            inputStream = socket.getInputStream();
-            outputStream = socket.getOutputStream();
-        } catch (MalformedURLException e) {
-            System.out.println(e.getMessage());
-        } catch (UnknownHostException e) {
-            System.out.println(e.getMessage());
-        } catch (IOException e) {
+        url = new URL(endpoint);
+        } catch(IOException e) {
             System.out.println(e.getMessage());
         }
     }
