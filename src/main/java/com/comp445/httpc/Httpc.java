b148493bc4ca6248dd41d67a7e82fd6992b70f4a
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-09-29 01:33:36 -0400

    Add documentation

--- src/main/java/com/comp445/httpc/Httpc.java
+++ src/main/java/com/comp445/httpc/Httpc.java
@@ -13,17 +13,20 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class Httpc {
+    // General usage instructions
     private String usageGeneral = "Usage:\n".concat("   httpc command [arguments]\n").concat("The commands are:\n")
             .concat("   get     executes a HTTP GET request and prints the response.\n")
             .concat("   post    executes a HTTP POST request and prints the response.\n")
             .concat("   help    prints this screen.\n\n")
             .concat("Use \"httpc help [command]\" for more information about a command.");
 
+    // Usage of the get command
     private String usageGet = "httpc help get\n".concat("   usage: httpc get [-v] [-h key:value]* URL\n")
             .concat("Get executes a HTTP GET request for a given URL.\n")
             .concat("   -v      Prints the detail of the response such as protocol, status, and headers.\n")
             .concat("   -h key:value    Associates headers to HTTP Request with the format 'key:value'.");
 
+    // Usage of the post command
     private String usagePost = "httpc help post\n"
             .concat("    usage: httpc post [-v] [-h key:value]* [-d inline-data] [-f file] URL\n")
             .concat("Post executes a HTTP POST request for a given URL with inline data or from file.\n")
@@ -31,25 +34,56 @@ public class Httpc {
             .concat("   post    executes a HTTP POST request and prints the response.\n")
             .concat("   help    prints this screen.\n\n")
             .concat("Use \"httpc help [command]\" for more information about a command.");
+    // All valid actions
     final Set<String> validActions = Set.of("post", "get");
 
+    // Object that parses arguments provided through the CLI
     private final CommandLineParser parser = new DefaultParser();
+
+    // Contains all options
     private final Options options = new Options();
+
+    // Contains all parsed arguments
     private CommandLine cmdLine;
+
+    // Contains the raw arguments provided
     private String[] args;
 
+    // Request created using provided arguments
     protected HttpcRequest req;
+
+    // Existence of verbose option
     protected boolean verbose = false;
+
+    // Request headers
     protected Map<String, String> headers = new HashMap<String, String>();
+
+    // Request data
     protected String data;
+
+    // Request action
     protected String action;
+
+    // Request target
     protected String target;
 
+    /**
+     * Constructor.
+     * 
+     * @param args Arguments provided through CLI.
+     * @throws Exception
+     */
     public Httpc(final String[] args) throws Exception {
         this.args = args;
         parse();
     }
 
+    /**
+     * Represents whether a string is a valid request.
+     * 
+     * @param check String to check.
+     * @return boolean Whether the check is a request.
+     */
     private boolean isRequest(final String check) {
         if (check != null && !check.isEmpty() && (validActions.contains(check))) {
             return true;
@@ -88,6 +122,11 @@ public class Httpc {
 
     /**
      * Sets the `data` variable.
+    /**
+     * Loads a file's contents into a String.
+     * 
+     * @return String Contents of the file.
+     * @throws IOException
      */
     private void collectData() {
         this.data = cmdLine.hasOption("d") ? cmdLine.getOptionValue("d") : cmdLine.getOptionValue("f");
