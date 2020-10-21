76274b2addf2d96e3241156833d01b8234800b27
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-10-21 05:23:37 -0400

    chore: remove unused import

--- client/src/main/java/com/comp445/httpc/HttpcRequest.java
+++ client/src/main/java/com/comp445/httpc/HttpcRequest.java
@@ -6,7 +6,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.util.Formatter;
